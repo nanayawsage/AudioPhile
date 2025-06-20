1bc5f0f2a9cc59ad20178f9872c16fa739cb62cd
Your Name <121442921+nanayawsage@users.noreply.github.com>
2025-06-20 22:08:52 +0000

    Product component
    Worked on product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 // import React, { useState } from "react";
 // import { Link } from "react-router-dom";
-import Hero from "./Hero";
+// import Hero from "./Hero";
 import Navigation from './Navigation';
 import Footer from './Footer';
+import Product from './Product';
 
 const Home = () => {
   return (
     <>
       <Navigation />
-      <Hero />
+        <Product />
+      {/* <Hero /> */}
 
     <Footer />
     </>
